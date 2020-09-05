b22452a0d1a27503da070038464f01777bc9b2e9
Nicholas Bishop <nicholasbishop@gmail.com>
2020-09-05 04:12:25 -0400

    Implement Command and Error

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,170 @@
+use log::info;
+use std::ffi::OsString;
+use std::os::unix::ffi::OsStrExt;
+use std::path::PathBuf;
+use std::{fmt, io, process};
+
+/// Type of error.
+#[derive(Debug)]
+pub enum ErrorKind {
+    /// The command failed to launch (e.g. if the executable does not
+    /// exist.)
+    Launch(io::Error),
+
+    /// The command exited non-zero or due to a signal.
+    Exit(process::ExitStatus),
+}
+
+/// Error returned by `Command::run`.
+#[derive(Debug)]
+pub struct Error {
+    /// The command that caused the error.
+    pub command: Command,
+
+    /// The type of error.
+    pub kind: ErrorKind,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match &self.kind {
+            ErrorKind::Launch(err) => write!(
+                f,
+                "failed to launch '{}': {}",
+                self.command.command_line_lossy(),
+                err
+            ),
+            ErrorKind::Exit(err) => write!(
+                f,
+                "command '{}' failed: {}",
+                self.command.command_line_lossy(),
+                err
+            ),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Command {
+    pub executable: PathBuf,
+    pub args: Vec<OsString>,
+    pub dir: Option<PathBuf>,
+    pub log_command: bool,
+    pub print_command: bool,
+    pub check: bool,
+    pub capture: bool,
+    // TODO: add clear_env and env fields
+}
+
+impl Command {
+    /// Run the command.
+    ///
+    /// If capture is true, the command's output (stdout and stderr)
+    /// is returned along with the status. If not, the stdout and
+    /// stderr are empty.
+    ///
+    /// If the command fails to start an error is returned. If check
+    /// is set, an error is also returned if the command exits
+    /// non-zero or due to a signal.
+    ///
+    /// If log_command and/or print_command is true then the command
+    /// line is logged and/or printed before running it. If the
+    /// command fails the error is not logged or printed, but the
+    /// resulting error type implements Display and can be used for
+    /// this purpose.
+    pub fn run(&self) -> Result<process::Output, Error> {
+        let cmd_str = self.command_line_lossy();
+        if self.log_command {
+            info!("{}", cmd_str);
+        }
+        if self.print_command {
+            println!("{}", cmd_str);
+        }
+
+        let mut cmd: process::Command = self.into();
+        let out = if self.capture {
+            cmd.output().map_err(|err| Error {
+                command: self.clone(),
+                kind: ErrorKind::Launch(err),
+            })?
+        } else {
+            let status = cmd.status().map_err(|err| Error {
+                command: self.clone(),
+                kind: ErrorKind::Launch(err),
+            })?;
+            process::Output {
+                stdout: Vec::new(),
+                stderr: Vec::new(),
+                status,
+            }
+        };
+        if self.check && !out.status.success() {
+            return Err(Error {
+                command: self.clone(),
+                kind: ErrorKind::Exit(out.status),
+            });
+        }
+        Ok(out)
+    }
+
+    /// Format as a space-separated command line.
+    pub fn command_line(&self) -> OsString {
+        // TODO: add some quoting, e.g. if an arg has a space in it
+        let mut out = OsString::new();
+        out.push(&self.executable);
+
+        for arg in &self.args {
+            out.push(" ");
+            out.push(arg);
+        }
+        out
+    }
+
+    /// Format as a space-separated command line and convert to a string.
+    pub fn command_line_lossy(&self) -> String {
+        String::from_utf8_lossy(self.command_line().as_bytes()).into()
+    }
+}
+
+impl Default for Command {
+    fn default() -> Self {
+        Command {
+            executable: PathBuf::new(),
+            args: Vec::new(),
+            dir: None,
+            log_command: false,
+            print_command: true,
+            check: true,
+            capture: true,
+        }
+    }
+}
+
+impl From<&Command> for process::Command {
+    fn from(cmd: &Command) -> Self {
+        let mut out = process::Command::new(&cmd.executable);
+        out.args(&cmd.args);
+        if let Some(dir) = &cmd.dir {
+            out.current_dir(dir);
+        }
+        out
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::path::Path;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_basic() {
+        let cmd = Command {
+            executable: Path::new("true").into(),
+            ..Default::default()
+        };
+        let out = cmd.run().unwrap();
+        assert!(out.status.success());
     }
 }
